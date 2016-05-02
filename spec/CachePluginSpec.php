a9d59c1c2d1935a4cb0d686a589d4a433cf990ff
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2016-05-03 00:36:32 +0200

    Initial commit

---
+++ spec/CachePluginSpec.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace spec\Http\Client\Common\Plugin;
+
+use Http\Message\StreamFactory;
+use Http\Promise\FulfilledPromise;
+use PhpSpec\ObjectBehavior;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+
+class CachePluginSpec extends ObjectBehavior
+{
+    function let(CacheItemPoolInterface $pool, StreamFactory $streamFactory)
+    {
+        $this->beConstructedWith($pool, $streamFactory, ['default_ttl'=>60]);
+    }
+
+    function it_is_initializable(CacheItemPoolInterface $pool)
+    {
+        $this->shouldHaveType('Http\Client\Common\Plugin\CachePlugin');
+    }
+
+    function it_is_a_plugin()
+    {
+        $this->shouldImplement('Http\Client\Common\Plugin');
+    }
+
+    function it_caches_responses(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response, StreamInterface $stream)
+    {
+        $httpBody = 'body';
+        $stream->__toString()->willReturn($httpBody);
+        $stream->isSeekable()->willReturn(true);
+        $stream->rewind()->shouldBeCalled();
+
+        $request->getMethod()->willReturn('GET');
+        $request->getUri()->willReturn('/');
+        $response->getStatusCode()->willReturn(200);
+        $response->getBody()->willReturn($stream);
+        $response->getHeader('Cache-Control')->willReturn(array());
+        $response->getHeader('Expires')->willReturn(array());
+
+        $pool->getItem('e3b717d5883a45ef9493d009741f7c64')->shouldBeCalled()->willReturn($item);
+        $item->isHit()->willReturn(false);
+        $item->set(['response' => $response, 'body' => $httpBody])->willReturn($item)->shouldBeCalled();
+        $item->expiresAfter(60)->willReturn($item)->shouldBeCalled();
+        $pool->save($item)->shouldBeCalled();
+
+        $next = function (RequestInterface $request) use ($response) {
+            return new FulfilledPromise($response->getWrappedObject());
+        };
+
+        $this->handleRequest($request, $next, function () {});
+    }
+
+    function it_doesnt_store_failed_responses(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response)
+    {
+        $request->getMethod()->willReturn('GET');
+        $request->getUri()->willReturn('/');
+        $response->getStatusCode()->willReturn(400);
+        $response->getHeader('Cache-Control')->willReturn(array());
+        $response->getHeader('Expires')->willReturn(array());
+
+        $pool->getItem('e3b717d5883a45ef9493d009741f7c64')->shouldBeCalled()->willReturn($item);
+        $item->isHit()->willReturn(false);
+
+        $next = function (RequestInterface $request) use ($response) {
+            return new FulfilledPromise($response->getWrappedObject());
+        };
+
+        $this->handleRequest($request, $next, function () {});
+    }
+
+    function it_doesnt_store_post_requests(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response)
+    {
+        $request->getMethod()->willReturn('POST');
+        $request->getUri()->willReturn('/');
+
+        $next = function (RequestInterface $request) use ($response) {
+            return new FulfilledPromise($response->getWrappedObject());
+        };
+
+        $this->handleRequest($request, $next, function () {});
+    }
+
+
+    function it_calculate_age_from_response(CacheItemPoolInterface $pool, CacheItemInterface $item, RequestInterface $request, ResponseInterface $response, StreamInterface $stream)
+    {
+        $httpBody = 'body';
+        $stream->__toString()->willReturn($httpBody);
+        $stream->isSeekable()->willReturn(true);
+        $stream->rewind()->shouldBeCalled();
+
+        $request->getMethod()->willReturn('GET');
+        $request->getUri()->willReturn('/');
+        $response->getStatusCode()->willReturn(200);
+        $response->getBody()->willReturn($stream);
+        $response->getHeader('Cache-Control')->willReturn(array('max-age=40'));
+        $response->getHeader('Age')->willReturn(array('15'));
+        $response->getHeader('Expires')->willReturn(array());
+
+        $pool->getItem('e3b717d5883a45ef9493d009741f7c64')->shouldBeCalled()->willReturn($item);
+        $item->isHit()->willReturn(false);
+
+        // 40-15 should be 25
+        $item->set(['response' => $response, 'body' => $httpBody])->willReturn($item)->shouldBeCalled();
+        $item->expiresAfter(25)->willReturn($item)->shouldBeCalled();
+        $pool->save($item)->shouldBeCalled();
+
+        $next = function (RequestInterface $request) use ($response) {
+            return new FulfilledPromise($response->getWrappedObject());
+        };
+
+        $this->handleRequest($request, $next, function () {});
+    }
+}
---
+++ src/CachePlugin.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Http\Client\Common\Plugin;
+
+use Http\Client\Common\Plugin;
+use Http\Message\StreamFactory;
+use Http\Promise\FulfilledPromise;
+use Psr\Cache\CacheItemPoolInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Allow for caching a response.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+final class CachePlugin implements Plugin
+{
+    /**
+     * @var CacheItemPoolInterface
+     */
+    private $pool;
+
+    /**
+     * @var StreamFactory
+     */
+    private $streamFactory;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @param CacheItemPoolInterface $pool
+     * @param StreamFactory          $streamFactory
+     * @param array                  $config        {
+     *
+     *     @var bool $respect_cache_headers Whether to look at the cache directives or ignore them.
+     *     @var int $default_ttl If we do not respect cache headers or can't calculate a good ttl, use this value.
+     * }
+     */
+    public function __construct(CacheItemPoolInterface $pool, StreamFactory $streamFactory, array $config = [])
+    {
+        $this->pool = $pool;
+        $this->streamFactory = $streamFactory;
+
+        $optionsResolver = new OptionsResolver();
+        $this->configureOptions($optionsResolver);
+        $this->config = $optionsResolver->resolve($config);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleRequest(RequestInterface $request, callable $next, callable $first)
+    {
+        $method = strtoupper($request->getMethod());
+
+        // if the request not is cachable, move to $next
+        if ($method !== 'GET' && $method !== 'HEAD') {
+            return $next($request);
+        }
+
+        // If we can cache the request
+        $key = $this->createCacheKey($request);
+        $cacheItem = $this->pool->getItem($key);
+
+        if ($cacheItem->isHit()) {
+            // return cached response
+            $data = $cacheItem->get();
+            $response = $data['response'];
+            $response = $response->withBody($this->streamFactory->createStream($data['body']));
+
+            return new FulfilledPromise($response);
+        }
+
+        return $next($request)->then(function (ResponseInterface $response) use ($cacheItem) {
+            if ($this->isCacheable($response)) {
+                $bodyStream = $response->getBody();
+                $body = $bodyStream->__toString();
+                if ($bodyStream->isSeekable()) {
+                    $bodyStream->rewind();
+                } else {
+                    $response = $response->withBody($this->streamFactory->createStream($body));
+                }
+
+                $cacheItem->set(['response' => $response, 'body' => $body])
+                    ->expiresAfter($this->getMaxAge($response));
+                $this->pool->save($cacheItem);
+            }
+
+            return $response;
+        });
+    }
+
+    /**
+     * Verify that we can cache this response.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return bool
+     */
+    protected function isCacheable(ResponseInterface $response)
+    {
+        if (!in_array($response->getStatusCode(), [200, 203, 300, 301, 302, 404, 410])) {
+            return false;
+        }
+        if (!$this->config['respect_cache_headers']) {
+            return true;
+        }
+        if ($this->getCacheControlDirective($response, 'no-store') || $this->getCacheControlDirective($response, 'private')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Get the value of a parameter in the cache control header.
+     *
+     * @param ResponseInterface $response
+     * @param string            $name     The field of Cache-Control to fetch
+     *
+     * @return bool|string The value of the directive, true if directive without value, false if directive not present.
+     */
+    private function getCacheControlDirective(ResponseInterface $response, $name)
+    {
+        $headers = $response->getHeader('Cache-Control');
+        foreach ($headers as $header) {
+            if (preg_match(sprintf('|%s=?([0-9]+)?|i', $name), $header, $matches)) {
+
+                // return the value for $name if it exists
+                if (isset($matches[1])) {
+                    return $matches[1];
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param RequestInterface $request
+     *
+     * @return string
+     */
+    private function createCacheKey(RequestInterface $request)
+    {
+        return md5($request->getMethod().' '.$request->getUri());
+    }
+
+    /**
+     * Get a ttl in seconds. It could return null if we do not respect cache headers and got no defaultTtl.
+     *
+     * @param ResponseInterface $response
+     *
+     * @return int|null
+     */
+    private function getMaxAge(ResponseInterface $response)
+    {
+        if (!$this->config['respect_cache_headers']) {
+            return $this->config['default_ttl'];
+        }
+
+        // check for max age in the Cache-Control header
+        $maxAge = $this->getCacheControlDirective($response, 'max-age');
+        if (!is_bool($maxAge)) {
+            $ageHeaders = $response->getHeader('Age');
+            foreach ($ageHeaders as $age) {
+                return $maxAge - ((int) $age);
+            }
+
+            return $maxAge;
+        }
+
+        // check for ttl in the Expires header
+        $headers = $response->getHeader('Expires');
+        foreach ($headers as $header) {
+            return (new \DateTime($header))->getTimestamp() - (new \DateTime())->getTimestamp();
+        }
+
+        return $this->config['default_ttl'];
+    }
+
+    /**
+     * Configure an options resolver.
+     *
+     * @param OptionsResolver $resolver
+     */
+    private function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'default_ttl' => null,
+            'respect_cache_headers' => true,
+        ]);
+
+        $resolver->setAllowedTypes('default_ttl', ['int', 'null']);
+        $resolver->setAllowedTypes('respect_cache_headers', 'bool');
+    }
+}
