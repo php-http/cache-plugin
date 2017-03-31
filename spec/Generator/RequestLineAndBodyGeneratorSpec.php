2690ae100610aca58654dd9dcd4f376646889233
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-31 18:33:52 +0200

    cs

--- spec/Generator/RequestLineAndBodyGeneratorSpec.php
+++ spec/Generator/RequestLineAndBodyGeneratorSpec.php
@@ -3,23 +3,21 @@
 namespace spec\Http\Client\Common\Plugin\Generator;
 
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Psr\Http\Message\RequestInterface;
 
 class RequestLineAndBodyGeneratorSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('Http\Client\Common\Plugin\Generator\RequestLineAndBodyGenerator');
     }
 
-    function it_is_a_key_generator()
+    public function it_is_a_key_generator()
     {
         $this->shouldImplement('Http\Client\Common\Plugin\Generator\CacheKeyGenerator');
     }
 
-
-    function it_generates_cache_from_request(RequestInterface $request)
+    public function it_generates_cache_from_request(RequestInterface $request)
     {
         $request->getMethod()->shouldBeCalled()->willReturn('GET');
         $request->getUri()->shouldBeCalled()->willReturn('http://example.com/foo');
@@ -28,7 +26,7 @@ class RequestLineAndBodyGeneratorSpec extends ObjectBehavior
         $this->generate($request)->shouldReturn('GET http://example.com/foo bar');
     }
 
-    function it_generates_cache_from_request_with_no_body(RequestInterface $request)
+    public function it_generates_cache_from_request_with_no_body(RequestInterface $request)
     {
         $request->getMethod()->shouldBeCalled()->willReturn('GET');
         $request->getUri()->shouldBeCalled()->willReturn('http://example.com/foo');
