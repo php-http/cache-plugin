c6dd7d6680e6516bb2c2c0aa4b433877b9406b75
Tobias Nyholm <tobias.nyholm@gmail.com>
2017-03-31 23:43:45 +0200

    Remove final

--- src/Cache/Generator/SimpleGenerator.php
+++ src/Cache/Generator/SimpleGenerator.php
@@ -9,7 +9,7 @@ use Psr\Http\Message\RequestInterface;
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
-final class SimpleGenerator implements CacheKeyGenerator
+class SimpleGenerator implements CacheKeyGenerator
 {
     public function generate(RequestInterface $request)
     {
